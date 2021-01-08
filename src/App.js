6b0c8bbeef7eee4c12d2a91d44b1d0497b9aec4d
Javier Manzano <javier.manzano22@gmail.com>
2021-01-08 12:04:27 +0100

    clean code

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ const App = () => {
       <header className="App-header">
         <h1>REVERSE YOUR FAVORITE WORD!</h1>
         <img src={logo} className="App-logo" />
-        <input type="text" onChange={(e) => setText(e?.target?.value)}/>
-        <span style={{ marginTop: 30, color: 'cyan'}}>Reversed text is.... "{reverseString(text)}"</span>
+        <input type="text" onChange={(e) => setText(e?.target?.value)} />
+        <span style={{ marginTop: 30, color: 'cyan' }}>Reversed text is.... "{reverseString(text)}"</span>
       </header>
     </div>
   );
