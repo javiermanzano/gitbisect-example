d4888585e2eceff2efa1f50a62750397cc191b4a
Javier Manzano <javier.manzano22@gmail.com>
2021-01-08 11:59:29 +0100

    just copy text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState('');
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <input type="text" />
+        <input type="text" onChange={(e) => setText(e?.target?.value)}/>
+        <span>Text should be.... {text}</span>
       </header>
     </div>
   );
