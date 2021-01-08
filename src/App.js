88f35d14948237b0bde6edb580ed3b1f731da67c
Javier Manzano <javier.manzano22@gmail.com>
2021-01-08 12:01:06 +0100

    working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
+function reverseString(str) {
+  var splitString = str.split("");
+  var reverseArray = splitString.reverse();
+  var joinArray = reverseArray.join("");
+  return joinArray;
+}
+
 function App() {
   const [text, setText] = useState('');
   return (
@@ -9,7 +16,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <input type="text" onChange={(e) => setText(e?.target?.value)}/>
-        <span>Text should be.... {text}</span>
+        <span>Text should be.... {reverseString(text)}</span>
       </header>
     </div>
   );
